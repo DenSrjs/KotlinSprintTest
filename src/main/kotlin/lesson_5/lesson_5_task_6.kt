209b24841a4e160ec768d8080e9547aadb55898e
denal <denalexsr727@gmail.com>
2025-06-13 20:15:45 +0300

    KS-5-6 changed 2

--- src/main/kotlin/lesson_5/lesson_5_task_6.kt
+++ src/main/kotlin/lesson_5/lesson_5_task_6.kt
@@ -1,13 +1,13 @@
 package org.example.lesson_5
 
-const val value_100 = 100
-const val value_18_5 = 18.5
-const val value_25_0 = 25.0
-const val value_30_0 = 30.0
+const val VALUE_100 = 100
+const val VALUE_18_5 = 18.5
+const val VALUE_25_0 = 25.0
+const val VALUE_30_0 = 30.0
 
 fun main() {
     print("Введите рост пользователя в см: ")
-    val userGrownMetre = readln().toFloat() / value_100
+    val userGrownMetre = readln().toFloat() / VALUE_100
 
     print("Введите вес пользователя в кг: ")
     val userWeight = readln().toFloat()
@@ -15,12 +15,11 @@ fun main() {
     val bodyMassIndex = userWeight / (userGrownMetre * userGrownMetre)
 
     val textResult = when {
-        (bodyMassIndex < value_18_5) -> "недостаточная масса тела"
-        (bodyMassIndex < value_25_0) -> "нормальная масса тела"
-        (bodyMassIndex < value_30_0) -> "избыточная масса тела"
+        (bodyMassIndex < VALUE_18_5) -> "недостаточная масса тела"
+        (bodyMassIndex < VALUE_25_0) -> "нормальная масса тела"
+        (bodyMassIndex < VALUE_30_0) -> "избыточная масса тела"
         else ->"ожирение"
     }
 
-
-    println("\nИндекс массы тела = ${String.format("%.2f", bodyMassIndex)} \nKатегория веса: $textResult")
+    println("\nИндекс массы тела = ${"%.2f".format(bodyMassIndex)} \nKатегория веса: $textResult")
 }
